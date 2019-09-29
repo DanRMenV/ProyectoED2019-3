29466697c5f3741e97c024da8c23d2eea8929169
Mendivelso <Mendivelso@Mendivelso-PC>
2019-09-29 11:12:41 -0500

    solved errors

--- ProyectoED/src/business/Execute.java
+++ ProyectoED/src/business/Execute.java
@@ -29,26 +29,7 @@ public class Execute {
   static void pantalla_inicio() {
 		EstudiantesManager em=new EstudiantesManager();
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Ingrese id");
-		String id=sc.nextLine();
-		int numId=Integer.parseInt(id);
-		System.out.println("Ingrese nombre");
-		String name=sc.nextLine();
-		System.out.println("Ingrese Apellido");
-		String last=sc.nextLine();
-		System.out.println("Ingrese dia de nacimiento");
-		String dia=sc.nextLine();
-		int numDia=Integer.parseInt(dia);
-		System.out.println("Ingrese mes de nacimiento");
-		String mes=sc.nextLine();
-		int numMes=Integer.parseInt(mes);
-		System.out.println("Ingrese a�o de nacimiento");
-		String a�o=sc.nextLine();
-		int numA�o=Integer.parseInt(a�o);
-		Calendar fecha=new GregorianCalendar(numA�o,numMes,numDia);
-		em.addEstudiante(new Estudiante(numId,name,last,fecha));
-		em.addEstudiante(new Estudiante(numId+1,name+"2",last,fecha));
-		
+				
 		
 		System.out.println("Escriba id a buscar");
 		String search=sc.nextLine();
