2fd855454b1ad806e968ef9e61b72fcb306987c8
Mendivelso <Mendivelso@Mendivelso-PC>
2019-11-12 19:43:00 -0500

    data and tree

--- ProyectoED/src/business/CursoManager.java
+++ ProyectoED/src/business/CursoManager.java
@@ -1,6 +1,12 @@
 package business;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
 import data.Curso;
+import data.Estudiante;
 import data.EstudianteBST;
 import data.ListCurso;
 
@@ -19,6 +25,56 @@ public class CursoManager {
     public  Curso FindCurso(int id) {
         return lista_curso.FindCurso(id);
     }
+    
+    /*public void readStudents(String fileName) {
+        int i=1;
+        try {
+            Scanner reader = new Scanner(new File(fileName));
+            while(reader.hasNextLine()) {
+                reader.useDelimiter(",");
+                String name = reader.next();
+                String surname = reader.next();
+                int curso = Integer.parseInt(reader.next());
+                int dia = (int) (Math.random() * 31) + 1;
+                int mes = (int) (Math.random() * 12) + 1;
+                int año = (int) (Math.random() * 2018) + 2000;
+                this.lista_curso.FindCurso(curso).est_curso.addEstudianteBST(i++,name,surname,dia,mes,año,curso);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    } */
+    
+    public void readStudents(String fileName) {
+		int i=1;
+		try {
+			Scanner reader = new Scanner(new File(fileName));
+			while(reader.hasNextLine()) {
+				String est = reader.nextLine();
+				createEstudiante(est,i++);
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void createEstudiante(String text,int id) {
+		Scanner sc = new Scanner(text);
+		sc.useDelimiter(",");
+		String name = sc.next();
+		String surname = sc.next();		
+		int curso = Integer.parseInt(sc.next());
+		sc.close();
+		int dia = (int) (Math.random() * 31) + 1;
+        int mes = (int) (Math.random() * 12) + 1;
+        int año = (int) (Math.random() * 2018) + 2000;
+        this.lista_curso.FindCurso(curso).est_curso.addEstudianteBST(id,name,surname,dia,mes,año,curso);
+	}
+    
     
     
 
